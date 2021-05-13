ffd4cccea92af778b989404fb4e58f890a120875
Matheus Cirilo <matheuscirilodasilva@gmail.com>
2021-05-13 11:22:25 -0300

    updeted Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,38 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+      title: "Contador de Pessoas",
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("Pessoas: 0",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: FlatButton(
+                    onPressed: () {},
+                    child: Text("+1",
+                        style: TextStyle(color: Colors.white, fontSize: 40.0))),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: FlatButton(
+                    onPressed: () {},
+                    child: Text("-1",
+                        style: TextStyle(color: Colors.white, fontSize: 40.0))),
+              ),
+            ],
+          ),
+          Text("Pode Entrar",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0)),
+        ],
+      )));
+}
